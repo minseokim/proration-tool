51d460d39a8779c625270cffb24146f21a338cc9
minseokim <minseoalexkim@gmail.com>
2020-12-05 16:49:55 +0900

    Implement basic layout

--- client/src/components/ProrationTool/ProrationTool.tsx
+++ client/src/components/ProrationTool/ProrationTool.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { ProrationBuilder } from '../ProrationBuilder';
 import { ProrationSummary } from '../ProrationSummary';
+import './ProrationTool.css';
+import { Row, Col } from 'antd';
+
 export const ProrationTool = () => {
   return (
-    <div>
-      <h1>ProrationTool</h1>
-      <ProrationBuilder />
-      <ProrationSummary />
+    <div className='container'>
+      <Row>
+        <Col flex={7}>
+          <ProrationBuilder />
+        </Col>
+        <Col flex={3}>
+          <ProrationSummary />
+        </Col>
+      </Row>
     </div>
   );
 };
